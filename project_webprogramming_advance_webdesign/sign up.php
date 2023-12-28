a62f95b66fa411485c5d3521474afca151b052e2
kokzhenchen0315 <kokzhenchen0315@e.newera.edu.my>
2023-12-28 11:05:00 +0800

    update html to php

--- project_webprogramming_advance_webdesign/sign up.php
+++ project_webprogramming_advance_webdesign/sign up.php
@@ -1,3 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "e_commerce_system";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
